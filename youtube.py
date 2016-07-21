f7409e9b6203440ed2dbaea66c89ed0bb78be5ae
Tommy Kelly <git@tkel.ly>
2016-07-21 10:15:19 -0700

    Only log errors and change routing to accept a video ID

--- youtube.py
+++ youtube.py
@@ -8,13 +8,25 @@ import youtube_dl
 app = Flask(__name__)
 
 
-@app.route('/')
-def get_info():
+class ErrorLogger(object):
+    def debug(self, msg):
+        pass
+
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        print(msg)
+
+
+@app.route('/metadata/<vid>')
+def get_info(vid):
 
     def get_best_audio(formats):
         best = {}
         for f in formats:
             # abr not in some formats with both a+v
+            # but we have already filtered those out so no need to worry
             if (not f['acodec'] in best or
                     best[f['acodec']]['abr'] > f['abr']):
                 best[f['acodec']] = f
@@ -23,10 +35,11 @@ def get_info():
     def has_av(f):
         return f['acodec'] != 'none' and f['vcodec'] != 'none'
 
-    ydl_opts = {}
+    ydl_opts = {'logger': ErrorLogger()}
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        info = ydl.extract_info('http://www.youtube.com/watch?v=BaW_jenozKc',
-                                download=False)
+        info = ydl.extract_info(
+                    'http://www.youtube.com/watch?v={}'.format(vid),
+                    download=False)
         audio_only = [f for f in info['formats'] if f['acodec'] != 'none' and
                       f['vcodec'] == 'none']
         audio_only_best = get_best_audio(audio_only)
