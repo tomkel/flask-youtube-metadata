0dd06a1858ac542a80156f3dc077be7607c9b4a7
Tommy Kelly <git@tkel.ly>
2016-07-24 04:37:12 -0700

    Add title in result json

--- youtube.py
+++ youtube.py
@@ -54,5 +54,6 @@ def get_info(vid):
         audio_only = [f for f in info['formats'] if f['acodec'] != 'none' and
                       f['vcodec'] == 'none']
         audio_only_best = get_best_audio(audio_only)
-        return jsonify([f for f in info['formats'] if has_av(f)] +
-                       audio_only_best)
+        return jsonify({'formats': [f for f in info['formats'] if has_av(f)] +
+                        audio_only_best,
+                       'title': info['title']})
