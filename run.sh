297bba52f85cd0df1062dfab0083c2c542d44fc8
Tommy Kelly <git@tkel.ly>
2019-09-17 22:52:06 -0700

    Revert "Change flags to work with latest uwsgi"
    This reverts commit bdac8065eca66738c1c3487836368e7ab82f5f07.

--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-uwsgi --socket :5000 --wsgi-file youtube.py --callable app --master --die-on-term --manage-script-name --stats :1717 --processes 4 --threads 2
-    # --http-socket
